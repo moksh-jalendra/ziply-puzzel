f4bc2737ab8e962a477835ca86d7cd9ec32f3a0e
moksh jalendra <mokshjalendra33@gmail.com>
2026-03-14 20:01:21 +0530

    builded level controll and beter ui flow

--- script.js
+++ script.js
@@ -1,128 +1,352 @@
-const grid = document.getElementById("grid")
+//   some variables 
 let path = []
 let expected = 1
-
-const puzzle = [
-    1,0,0,0,2,
-    0,0,0,0,0,
-    0,0,3,0,0,
-    0,0,0,0,0,
-    4,0,0,0,5
-]
-let puzzelindex = 0
+let selectedLevelkey = 'level1'
 
 
-
-
-
-for(let x = 0 ; x < 5 ; x ++ ){
-
-    for(let y = 0 ; y < 5 ; y ++ ){
-        let cell = document.createElement("div")
-        cell.classList.add("cell")
-        cell.dataset.x = x
-        cell.dataset.y= y
-
-        if (puzzle[puzzelindex]!== 0){
-            cell.innerText = puzzle[puzzelindex]
-        }
-
-        grid.appendChild(cell)
-        puzzelindex ++
-    }
-    
+const levels ={
+    level1 :
+    [
+        1,0,0,0,2,
+        0,0,0,0,0,
+        0,0,3,0,0,
+        0,0,0,0,0,
+        4,0,0,0,5
+    ],
+    level2 :
+    [
+        1,0,0,0,2,
+        0,0,0,0,0,
+        0,0,3,0,0,
+        0,0,0,0,6,
+        4,0,0,0,5
+    ],
+    level3 :
+    [
+        1,0,0,0,2,
+        0,0,0,0,0,
+        6,0,3,0,0,
+        5,0,0,0,0,
+        4,0,0,0,0
+    ]
 }
 
 
 
-grid.addEventListener( "touchmove", function(e) {
-    e.preventDefault()
-   
-    let touch = e.touches[0]
-    x = touch.clientX
-    y = touch.clientY
-    
-    let ellement = document.elementFromPoint(touch.clientX , touch.clientY)
-    if (ellement && ellement.classList.contains('cell')){
-        let gridx = ellement.dataset.x
-        let gridy = ellement.dataset.y
-        let coordinate = gridx + "-" + gridy
-        if(path.length === 0 ){
-            if(ellement.innerText !== "1"){
-                return
+
+//        level logic 
+ 
+ const LevlDisplay = document.getElementById('level-disply')
+ for(let [key,value] of Object.entries(levels)){
+    let levelBox = document.createElement('div')
+    levelBox.classList.add('levelbox')
+    levelBox.dataset.val= value
+    levelBox.innerText= key
+    LevlDisplay.appendChild(levelBox)
+    if(key === selectedLevelkey ){
+        levelBox.style.backgroundColor = 'orange'
+
+    }
+
+    levelBox.addEventListener('click',function(){
+        document.querySelectorAll('.levelbox').forEach(box => {
+            box.style.backgroundColor = 'white'
+        })
+        levelBox.style.backgroundColor = 'orange'
+        selectedLevelkey = key 
+    })
+ }
+ 
+ 
+
+ //              main zip logic             
+
+function zipLogic(puzzleArry){
+    const grid = document.getElementById("grid")
+    grid.innerHTML = ''
+    path = []
+    expected = 1
+
+    let puzzle = puzzleArry
+
+    let puzzelindex = 0
+
+
+    for (let x = 0; x < 5; x++) {
+
+        for (let y = 0; y < 5; y++) {
+            let cell = document.createElement("div")
+            cell.classList.add("cell")
+            cell.dataset.x = x
+            cell.dataset.y = y
+
+            if (puzzleArry[puzzelindex] !== 0) {
+                cell.innerText = puzzleArry[puzzelindex]
             }
-            
+
+            grid.appendChild(cell)
+            puzzelindex++
         }
-        if (path.length > 0 ){
 
-            let lastcoordinate = path[path.length - 1]
-            let lastx = parseInt(lastcoordinate.split("-")[0])
-            let lasty = parseInt(lastcoordinate.split("-")[1])
+    }
 
-            let currentx = parseInt(gridx)
-            let currenty = parseInt(gridy)
 
-            let distancex = Math.abs(currentx - lastx)
-            let distancey = Math.abs(currenty - lasty)
-            
-            if(distancex + distancey !== 1){
-                return
-            }
 
-        }
-        if(path.length >1 ){
-            if(path[path.length -2] === coordinate){
-                let removdbox = path.pop()
-                let removedx = removdbox.split('-')[0]
-                let removedy = removdbox.split("-")[1]
+    grid.addEventListener("touchmove", function (e) {
+        e.preventDefault()
 
-                let celltoerase = document.querySelector(`[data-x="${removedx}"][data-y="${removedy}"]`)
-                celltoerase.style.backgroundColor=''
-                if(celltoerase.innerText !==''){
-                    expected --
+        let touch = e.touches[0]
+        x = touch.clientX
+        y = touch.clientY
+
+        let ellement = document.elementFromPoint(touch.clientX, touch.clientY)
+        if (ellement && ellement.classList.contains('cell')) {
+            let gridx = ellement.dataset.x
+            let gridy = ellement.dataset.y
+            let coordinate = gridx + "-" + gridy
+            if (path.length === 0) {
+                if (ellement.innerText !== "1") {
+                    return
                 }
-                return
+
             }
-        }
-        if(ellement.innerText !== ''){
-            let cellnumber = parseInt(ellement.innerText)
-            if(cellnumber !== expected){
-                return
+            if (path.length > 0) {
+
+                let lastcoordinate = path[path.length - 1]
+                let lastx = parseInt(lastcoordinate.split("-")[0])
+                let lasty = parseInt(lastcoordinate.split("-")[1])
+
+                let currentx = parseInt(gridx)
+                let currenty = parseInt(gridy)
+
+                let distancex = Math.abs(currentx - lastx)
+                let distancey = Math.abs(currenty - lasty)
+
+                if (distancex + distancey !== 1) {
+                    return
+                }
+
             }
-            expected++
+            if (path.length > 1) {
+                if (path[path.length - 2] === coordinate) {
+                    let removdbox = path.pop()
+                    let removedx = removdbox.split('-')[0]
+                    let removedy = removdbox.split("-")[1]
+
+                    let celltoerase = document.querySelector(`[data-x="${removedx}"][data-y="${removedy}"]`)
+                    celltoerase.style.backgroundColor = ''
+                    if (celltoerase.innerText !== '') {
+                        expected--
+                    }
+                    return
+                }
+            }
+            if (ellement.innerText !== '') {
+                let cellnumber = parseInt(ellement.innerText)
+                if (cellnumber !== expected) {
+                    return
+                }
+                expected++
+            }
+            if (!path.includes(coordinate)) {
+                path.push(coordinate)
+                ellement.style.backgroundColor = 'orange'
+            }
+            if (path.length === 25) {
+                alert("congrutulaton you won")
+            }
+
         }
-        if(!path.includes(coordinate)){
-            path.push(coordinate)
-            ellement.style.backgroundColor = 'orange'
-        }
-        if(path.length === 25){
-            alert("congrutulaton you won")
-        }
-       
-    }
 
-})
+    })
 
-
-function startlevel(val){
-
-
-    // herer we wright level code
     
-    // here we wright common code 
-
-    let home = document.getElementById('home-screen')
-    let game = document.getElementById('game-screen')
-    home.style.display = 'none'
-    game.style.display='flex'
 }
-function gohome(){
-    let home = document.getElementById('home-screen')
-    let game = document.getElementById('game-screen')
+
+
+//             page togle logic 
+
+
+
+let levl = document.getElementById("level-screen")
+let home = document.getElementById("home-screen")
+let game = document.getElementById("game-screen")
+function slideTheme(direction){
+    let slider = document.getElementById('theme-slider')
+    let scrollAmount = slider.clientWidth/2
+
+    slider.scrollBy({
+        left:scrollAmount*direction,
+        behavior:"smooth"
+    })
+}
+
+
+function OpenLevelPage(){
+    
+    levl.style.display = 'flex'
+    home.style.display= 'none'
+    game.style.display ='none'
+
+}
+
+function start(){
+    levl.style.display = 'none'
+    game.style.display = 'flex'
+    let chosenPuzzel = levels[selectedLevelkey]
+    zipLogic(chosenPuzzel)
+
+}
+
+function backhome(){
+    levl.style.display = 'none'
     home.style.display = 'flex'
-    game.style.display='none'
-
 }
 
 
+
+
+
+
+//        mani zip logic over here 
+
+
+
+
+
+//     older ui  code        
+
+
+// function startlevel(val){
+
+
+//     // herer we wright level code
+    
+//     // here we wright common code 
+
+//     let home = document.getElementById('home-screen')
+//     let game = document.getElementById('game-screen')
+//     home.style.display = 'none'
+//     game.style.display='flex'
+// }
+
+
+
+
+
+// function gohome(){
+//     let home = document.getElementById('home-screen')
+//     let game = document.getElementById('game-screen')
+//     home.style.display = 'flex'
+//     game.style.display='none'
+
+// }
+
+
+
+// const grid = document.getElementById("grid")
+// let path = []
+// let expected = 1
+
+// const puzzle = [
+//     1,0,0,0,2,
+//     0,0,0,0,0,
+//     0,0,3,0,0,
+//     0,0,0,0,0,
+//     4,0,0,0,5
+// ]
+
+// let puzzelindex = 0
+
+
+
+
+
+// for(let x = 0 ; x < 5 ; x ++ ){
+
+//     for(let y = 0 ; y < 5 ; y ++ ){
+//         let cell = document.createElement("div")
+//         cell.classList.add("cell")
+//         cell.dataset.x = x
+//         cell.dataset.y= y
+
+//         if (puzzle[puzzelindex]!== 0){
+//             cell.innerText = puzzle[puzzelindex]
+//         }
+
+//         grid.appendChild(cell)
+//         puzzelindex ++
+//     }
+    
+// }
+
+
+
+// grid.addEventListener( "touchmove", function(e) {
+//     e.preventDefault()
+   
+//     let touch = e.touches[0]
+//     x = touch.clientX
+//     y = touch.clientY
+    
+//     let ellement = document.elementFromPoint(touch.clientX , touch.clientY)
+//     if (ellement && ellement.classList.contains('cell')){
+//         let gridx = ellement.dataset.x
+//         let gridy = ellement.dataset.y
+//         let coordinate = gridx + "-" + gridy
+//         if(path.length === 0 ){
+//             if(ellement.innerText !== "1"){
+//                 return
+//             }
+            
+//         }
+//         if (path.length > 0 ){
+
+//             let lastcoordinate = path[path.length - 1]
+//             let lastx = parseInt(lastcoordinate.split("-")[0])
+//             let lasty = parseInt(lastcoordinate.split("-")[1])
+
+//             let currentx = parseInt(gridx)
+//             let currenty = parseInt(gridy)
+
+//             let distancex = Math.abs(currentx - lastx)
+//             let distancey = Math.abs(currenty - lasty)
+            
+//             if(distancex + distancey !== 1){
+//                 return
+//             }
+
+//         }
+//         if(path.length >1 ){
+//             if(path[path.length -2] === coordinate){
+//                 let removdbox = path.pop()
+//                 let removedx = removdbox.split('-')[0]
+//                 let removedy = removdbox.split("-")[1]
+
+//                 let celltoerase = document.querySelector(`[data-x="${removedx}"][data-y="${removedy}"]`)
+//                 celltoerase.style.backgroundColor=''
+//                 if(celltoerase.innerText !==''){
+//                     expected --
+//                 }
+//                 return
+//             }
+//         }
+//         if(ellement.innerText !== ''){
+//             let cellnumber = parseInt(ellement.innerText)
+//             if(cellnumber !== expected){
+//                 return
+//             }
+//             expected++
+//         }
+//         if(!path.includes(coordinate)){
+//             path.push(coordinate)
+//             ellement.style.backgroundColor = 'orange'
+//         }
+//         if(path.length === 25){
+//             alert("congrutulaton you won")
+//         }
+       
+//     }
+
+// })
 
